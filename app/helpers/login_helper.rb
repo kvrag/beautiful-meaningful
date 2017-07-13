1b3349273faa00818d2515ab1436872bf45f50b9
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-13 12:10:23 -0500

    Add app folder to skeleton

---
+++ app/helpers/login_helper.rb
@@ -0,0 +1,23 @@
+module LoginHelper
+
+  def log_in(args = {})
+    user = User.find_by_email(args[:email])
+    if user.nil?
+      false
+    elsif user.password == args[:password]
+      session[:user_id] = user.id
+      true
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+end
+
+helpers LoginHelper
