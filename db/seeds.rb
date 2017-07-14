b4e7d4897844229f7ffe906814106d80e6264b3d
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-13 19:47:55 -0500

    Add feelings and activities migrations and models, create seed data for feelings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 # SEED FILE
+
+Feeling.new({name: "stress"})
+Feeling.new({name: "anger"})
+Feeling.new({name: "anxious"})
+Feeling.new({name: "disgust"})
+Feeling.new({name: "tired"})
+Feeling.new({name: "fear"})
+Feeling.new({name: "sadness"})
+Feeling.new({name: "joy"})
+
